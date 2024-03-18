5be4d87c763767141bee304bd8b610e3db3425f1
FestusMusili <fessycompass@gmail.com>
2024-03-18 15:10:54 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 root@6963f7bb2ede:~/alx-higher_level_programming/0x03-python-data_structures# cat 1-element_at.py
 #!/usr/bin/python3
 def element_at(my_list, idx):
-if idx < 0:
-return (None)
-if idx > len(my_list) - 1:
-return (None)
-return my_list[idx]
+    if idx < 0:
+        return (None)
+    if idx > len(my_list) - 1:
+        return (None)
+    return my_list[idx]
